bc6337dd94c379f889fa1d1eaaba43448335de25
enisn <iletisim@enisnecipoglu.com>
2018-03-26 12:04:11 +0300

    Add project files.

---
+++ Manual.ColorPickerSample/Manual.ColorPickerSample/Controls/ColorPicker.cs
@@ -0,0 +1,37 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Manual.ColorPickerSample.Controls
+{
+    public class ColorPicker
+    {
+
+
+        static readonly Color[] defaultColors = { Color.Blue, Color.Green, Color.Red, Color.Yellow, Color.Orange, Color.Purple, Color.Brown };
+        /// <summary>
+        /// If user cancel it, it'll return null
+        /// </summary>
+        /// <param name="title">Title of yor dialog</param>
+        /// <param name="message">Message of your dialog</param>
+        /// <param name="colors">Colors can be picked from your dialog</param>
+        public static Task<Color?> GetColorAsync(string title, string message, params Color[] colors)
+        {
+            var cts = new TaskCompletionSource<Color?>();
+
+            if (colors == null || colors.Length == 0) colors = defaultColors;
+            var _dialogView = new ColorPickerView(title, message, colors);
+            _dialogView.Picked += (s,e)=> { cts.SetResult(_dialogView.CurrentColor); PopupNavigation.PopAsync(); };
+            PopupNavigation.PushAsync(new Rg.Plugins.Popup.Pages.PopupPage
+            {
+                Content = _dialogView
+            });
+
+            return cts.Task;
+        
+        }
+    }
+}
---
+++ Manual.ColorPickerSample/Manual.ColorPickerSample/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Manual.ColorPickerSample.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Manual.ColorPickerSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void ChooseColor_Clicked(object sender, EventArgs e)
+        {
+            var result = await ColorPicker.GetColorAsync("Primary Color", "Choose a color:");
+
+
+            if (result != null) //If result is null, user canceled to picking color
+            {
+                this.BindingContext = new { Color = result.Value };
+            }
+        }
+
+    }
+}
